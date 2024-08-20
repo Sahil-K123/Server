6e2a79ea74bd3a5cf160e13396327e4ea7b72d3c
Sahil-K123 <nikhilkarwani270@gmail.com>
2024-08-20 14:01:45 +0530

    including deployment url on other file for client side

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io('ws://localhost:3500')
+const socket = io('https://chaterbox.vercel.app/')
 
 const msgInput = document.querySelector('#message')
 const nameInput = document.querySelector('#name')
